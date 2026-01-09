930ccc1679f9431ac9c111e976bd489bf9ee193e
prashant1s <prashantextra2003@gmail.com>
2026-01-09 21:32:25 +0530

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Hero } from "@/sections/Hero";
+import { Navbar } from "@/layout/Navbar";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Resume } from "@/sections/Resume";
+import { Contact } from "@/sections/Contact";
+import { Footer } from "./layout/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      
+      <Navbar />
+      <main>
+        <Hero />
+        <Projects />
+        <About />
+        <Resume />
+        <Contact />
+      </main>
+    </div>
+  );
+}
+
+export default App;
